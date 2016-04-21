ddea8479bb3ff35937053d6ef37990a5fa660184
moo15z <mayaz.bb@gmail.com>
2016-04-21 11:26:45 +0900

    add strong para

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,39 @@
 class NotesController < ApplicationController
+	before_action :set_note, only: [:show, :edit, :update, :destroy]
+
   def new
   end
 
   def create
+  	@note = Note.new(note_params)
+  	@note.save
   end
+
+  def index
+  	@notes = Note.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private 
+
+ 		def set_note
+ 			@note = Note.find(params[:id])
+ 		end
+
+
+  	def note_params
+   		params.require(:note).permit(:title, :content)
+  	end
+
 end
